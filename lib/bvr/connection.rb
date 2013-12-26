775ab6bd0cb2b10419197d66fc913945042b2143
gregory <greg2502@gmail.com>
2013-12-25 17:36:08 -0800

    mv class methods, use accessor for easier specs

--- lib/bvr/connection.rb
+++ lib/bvr/connection.rb
@@ -6,23 +6,26 @@ module Bvr
     BASE_URI = 'https://www.voipinfocenter.com'
     API_PATH = '/API/Request.ashx?'
 
-    def self.base_uri
-      BASE_URI
-    end
+    attr_accessor :faraday_connection
 
-    def self.connection(faraday_adapter=Faraday.default_adapter)
-      Faraday.new(url: self.base_uri) do |faraday|
+    def initialize(faraday_adapter=Faraday.default_adapter)
+      @faraday_connection = Faraday.new(url: self.base_uri) do |faraday|
         faraday.response :logger
         faraday.adapter faraday_adapter
       end
     end
 
-    def self.get(params)
-      #TODO: prase body for 500
-      self.connection.get(self.uri(params)).body
+    def base_uri
+      BASE_URI
     end
 
-    def self.uri(queryH)
+
+    def get(params)
+      #TODO: prase body for 500
+      self.faraday_connection.get(uri(params)).body
+    end
+
+    def uri(queryH)
       params = {
         username: Bvr.config.username,
         password: Bvr.config.password
